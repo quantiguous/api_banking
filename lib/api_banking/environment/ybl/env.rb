ce397f1b418086c18f683fea15f076956040857b
akil <kataria.subs@gmail.com>
2016-06-14 11:41:57 +0530

    refactored, to allow for separate endpoints per service/environment

--- lib/api_banking/environment/ybl/env.rb
+++ lib/api_banking/environment/ybl/env.rb
@@ -1,18 +1,24 @@
 module ApiBanking
   module Environment
     module YBL
-      UAT = Struct.new(:user, :password, :client_id, :client_secret, :url) do
+      UAT = Struct.new(:user, :password, :client_id, :client_secret, :endpoints) do
         def initialize(*)
           super
-          self.url ||= 'https://uatsky.yesbank.in/app/uat/'
+          self.endpoints = {
+            FundsTransferByCustomerService:  'https://uatsky.yesbank.in/app/uat/fundsTransferByCustomerServiceHttpService',
+            FundsTransferByCustomerService2: 'https://uatsky.yesbank.in/app/uat/fundsTransferByCustomerService2',
+            InstantMoneyTransferService:     'https://uatsky.yesbank.in:7081/IMTService'
+          }
         end
       end
       
-      PRD = Struct.new(:user, :password, :client_id, :client_secret, :ssl_client_cert, :ssl_client_key, :ssl_ca_file, :url) do
+      PRD = Struct.new(:user, :password, :client_id, :client_secret, :ssl_client_cert, :ssl_client_key, :ssl_ca_file, :endpoints) do
         def initialize(*)
           super
           self.ssl_ca_file ||= File.expand_path('./prd.pem', File.dirname(__FILE__))
-          self.url ||= 'https://sky.yesbank.in:444/app/live/ssl'
+          self.endpoints = {
+            FundsTransferByCustomerService:  'https://sky.yesbank.in:444/app/live/ssl/fundsTransferByCustomerService'
+          }          
         end
       end
     end
