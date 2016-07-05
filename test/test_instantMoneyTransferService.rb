91d6a50e94ffeea5b617734969927f77f71c42ba
divyajayan <divya.jayan@quantiguous.com>
2016-07-05 10:46:50 +0530

    getBalance for FT2 and IMT addBeneficiary test fix

--- test/test_instantMoneyTransferService.rb
+++ test/test_instantMoneyTransferService.rb
@@ -3,35 +3,38 @@ require 'minitest_helper'
 class InstantMoneyTransferService < Minitest::Test
   
   def test_it_gives_back_a_initiateTransfer_result
-    
+
     request = ApiBanking::InstantMoneyTransferService::InitiateTransfer::Request.new()
-        
+
     request.uniqueRequestNo = SecureRandom.uuid.gsub!('-','')
     request.appID = 'app12345'
     request.customerID = '42989'
     request.beneficiaryMobileNo = '9637257928'
-    request.transferAmount = 2000
+    request.transferAmount = 100
     request.passCode = 1734
     request.remitterToBeneficiaryInfo = 'FUND TRANSFER'
-        
+
     puts "#{self.class.name} : #{ApiBanking::InstantMoneyTransferService.transfer(request)}"
-    
-  end  
+
+  end
 
   def test_it_gives_back_a_addBeneficiary_result
-    
+
     beneficiaryAddress = ApiBanking::InstantMoneyTransferService::AddBeneficiary::Address.new()
     request = ApiBanking::InstantMoneyTransferService::AddBeneficiary::Request.new()
-        
+
     request.uniqueRequestNo = SecureRandom.uuid.gsub!('-','')
     request.appID = 'app12345'
     request.customerID = '42989'
     request.beneficiaryMobileNo = '9538321404'
     request.beneficiaryName = 'Lavanya'
     beneficiaryAddress.addressLine = 'Mysore'
-        
+    beneficiaryAddress.cityName = 'Hootagali'
+    beneficiaryAddress.postalCode = '670307'
+    request.beneficiaryAddress = beneficiaryAddress
+
     puts "#{self.class.name} : #{ApiBanking::InstantMoneyTransferService.add_beneficiary(request)}"
-    
-  end  
+
+  end
   
 end
