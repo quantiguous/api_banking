7b01f2b9ce3f7285e4666f278b21b71a7fb3f7a4
akil <kataria.subs@gmail.com>
2016-09-01 16:55:55 +0530

    added prd url for ft2

--- lib/api_banking/environment/ybl/env.rb
+++ lib/api_banking/environment/ybl/env.rb
@@ -18,6 +18,7 @@ module ApiBanking
           self.ssl_ca_file ||= File.expand_path('./prd.pem', File.dirname(__FILE__))
           self.endpoints = {
             FundsTransferByCustomerService:  'https://sky.yesbank.in:444/app/live/ssl/fundsTransferByCustomerService'
+            FundsTransferByCustomerService2:  'https://sky.yesbank.in:444/app/live/fundsTransferByCustomerService2'
           }          
         end
       end
