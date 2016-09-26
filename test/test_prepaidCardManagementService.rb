5e9d4779e208d2f0210106125b7a973b267437b1
divyajayan <divya.jayan@quantiguous.com>
2016-09-26 13:25:26 +0530

    added PrepaidCardManagementService

---
+++ test/test_prepaidCardManagementService.rb
@@ -0,0 +1,69 @@
+require 'minitest_helper'
+
+class TestPrepaidCardManagementService < Minitest::Test
+  def test_prepaidCardManagementService_exists
+    assert ApiBanking::PrepaidCardManagementService
+  end
+  
+  def test_it_gives_back_a_register_card_result
+
+    address = ApiBanking::PrepaidCardManagementService::RegisterCard::Address.new()
+    idDocument = ApiBanking::PrepaidCardManagementService::RegisterCard::IDDocument.new()
+    request = ApiBanking::PrepaidCardManagementService::RegisterCard::Request.new()
+
+    address.addressLine1 = 'Shankar Lane'
+    address.addressLine2 = 'Kandivali'
+    address.city = 'Mumbai'
+    address.state = 'Maharashtra'
+    address.country = 'india'
+    address.postalCode = '400101'
+    
+    idDocument.documentType = 'passport'
+    idDocument.documentNo = Time.now.to_i
+    idDocument.countryOfIssue = 'india'
+    
+    request.uniqueRequestNo = SecureRandom.uuid.gsub!('-','')
+    request.appID = 'APP13'
+    request.title = 'Miss'
+    request.firstName = 'Divya'
+    request.lastName = 'Jayan'
+    request.preferredName = 'divya'
+    request.mobileNo = Time.now.to_i
+    request.gender = 'female'
+    request.nationality = 'indian'
+    request.birthDate = '1992-12-18'
+    request.idDocument = idDocument
+    request.address = address
+    request.proxyCardNumber = '000000418950'
+    # request.productCode = 'papp99'
+
+    puts "#{self.class.name} #{ApiBanking::PrepaidCardManagementService.registerCard(request)}"
+
+  end
+  
+  def test_it_gives_back_a_load_card_result
+    
+    request = ApiBanking::PrepaidCardManagementService::LoadCard::Request.new()
+    
+    request.uniqueRequestNo = SecureRandom.uuid.gsub!('-','')
+    request.appID = 'APP13'
+    request.customerID = '2424'
+    request.debitAccountNo = '001790700000090'
+    request.mobileNo = '9008888888'
+    request.loadAmount = '1000'
+    
+    puts "#{self.class.name} #{ApiBanking::PrepaidCardManagementService.loadCard(request)}"
+  end
+
+  def test_it_gives_back_a_block_card_result
+    
+    request = ApiBanking::PrepaidCardManagementService::BlockCard::Request.new()
+    
+    request.uniqueRequestNo = SecureRandom.uuid.gsub!('-','')
+    request.appID = 'APP13'
+    request.mobileNo = '9008888881'
+    
+    puts "#{self.class.name} #{ApiBanking::PrepaidCardManagementService.blockCard(request)}"
+  end
+
+end
