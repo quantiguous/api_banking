04770fb46b48dca1778f80167e398954a38c43f3
Awin Abi <awinabi@gmail.com>
2017-01-01 09:49:23 +0530

    Callbacks (#15)
    * Callbacks on remote call
    
    * Allow to execute a function before and after a Typhoeus::Request is
    * run. This can be used to hook in logging or other functions
    * Callbacks on json client
    * Fix specs
    
    * Add callbacks to other api calls
    
    * version bump

--- lib/api_banking/json/singlePayment.rb
+++ lib/api_banking/json/singlePayment.rb
@@ -1,8 +1,8 @@
 module ApiBanking
   class SinglePayment < JsonClient
-    
+
     SERVICE_VERSION = 1
-    
+
     attr_accessor :request, :result
 
     #transfer
@@ -11,32 +11,32 @@ module ApiBanking
     Request = Struct.new(:uniqueRequestNo, :corpID, :makerID, :checkerID, :approverID, :remitter, :beneficiary, :amount, :issueBranchCode, :modeOfPay, :remarks, :rptCode)
 
     Result = Struct.new(:status, :errorCode, :errorDescription)
-    
+
     class << self
       attr_accessor :configuration
     end
-        
+
     def self.configure
       self.configuration ||= Configuration.new
       yield(configuration)
     end
-    
+
     class Configuration
       attr_accessor :environment, :proxy, :timeout
     end
-        
-    def self.transfer(request)
+
+    def self.transfer(request, callbacks = nil)
       dataHash = {}
       dataHash[:Single_Payment_Corp_Req] = {}
       dataHash[:Single_Payment_Corp_Req][:Header] = {}
       dataHash[:Single_Payment_Corp_Req][:Body] = {}
-      
+
       dataHash[:Single_Payment_Corp_Req][:Header][:TranID] = request.uniqueRequestNo
       dataHash[:Single_Payment_Corp_Req][:Header][:Corp_ID] = request.corpID
       dataHash[:Single_Payment_Corp_Req][:Header][:Maker_ID] = request.makerID
       dataHash[:Single_Payment_Corp_Req][:Header][:Checker_ID] = request.checkerID
       dataHash[:Single_Payment_Corp_Req][:Header][:Approver_ID] = request.approverID
-      
+
       dataHash[:Single_Payment_Corp_Req][:Body][:Amount] = request.amount
       dataHash[:Single_Payment_Corp_Req][:Body][:Debit_Acct_No] = request.remitter.accountNo
       dataHash[:Single_Payment_Corp_Req][:Body][:Debit_Acct_Name] = request.remitter.accountName
@@ -44,7 +44,7 @@ module ApiBanking
       dataHash[:Single_Payment_Corp_Req][:Body][:Debit_Mobile] = request.remitter.mobileNo
       dataHash[:Single_Payment_Corp_Req][:Body][:Debit_TrnParticulars] = request.remitter.tranParticulars
       dataHash[:Single_Payment_Corp_Req][:Body][:Debit_PartTrnRmks] = request.remitter.partTranRemarks
-      
+
       dataHash[:Single_Payment_Corp_Req][:Body][:Ben_IFSC] = request.beneficiary.accountIFSC
       dataHash[:Single_Payment_Corp_Req][:Body][:Ben_Acct_No] = request.beneficiary.accountNo
       dataHash[:Single_Payment_Corp_Req][:Body][:Ben_Name] = request.beneficiary.fullName
@@ -61,15 +61,15 @@ module ApiBanking
       dataHash[:Single_Payment_Corp_Req][:Body][:Remarks] = request.remarks
       dataHash[:Single_Payment_Corp_Req][:Body][:RptCode] = request.rptCode
 
-            
-      reply = do_remote_call(dataHash)
-      
+
+      reply = do_remote_call(dataHash, callbacks)
+
       parse_reply(:transferResponse, reply)
     end
-  
-    
+
+
     private
-    
+
     def self.uri()
       if self.configuration.environment.kind_of?ApiBanking::Environment::RBL::UAT
         return '/test/sb/rbl/v1/payments/corp/payment'
@@ -77,7 +77,7 @@ module ApiBanking
         return '/sb/rbl/v1/payments/corp/payment'
       end
     end
- 
+
     def self.parse_reply(operationName, reply)
       if reply.kind_of?Fault
         return reply
--- lib/api_banking/soap/soap_client.rb
+++ lib/api_banking/soap/soap_client.rb
@@ -1,39 +1,42 @@
 module ApiBanking
-  
-  class SoapClient    
-    
+
+  class SoapClient
+
     def self.last_response
       Thread.current[:last_response]
     end
-         
-    def self.do_remote_call(env, soapAction = nil, &block)
+
+    def self.do_remote_call(env, callbacks = nil, soapAction = nil, &block)
       data = construct_envelope(&block)
       options = {}
       options[:method] = :post
       options[:body] = data.to_xml
-      
+
       # add soap11/12 specific headers
       add_soap_headers(options, soapAction)
-      
+
       # TODO: configuration removed for thread-safety, the assumption that one app at one time will connect to one environment
       # isn't valid anymore (starkit)
       # options[:proxy] = self.configuration.proxy
       # options[:timeout] = self.configuration.timeout
-      
+
       set_options_for_environment(env, options)
-      
+
       options[:headers]['User-Agent'] = "Quantiguous; API Banking, Ruby Gem #{ApiBanking::VERSION}"
-      
+
       request = Typhoeus::Request.new(env.endpoints[self.name.split('::').last.to_sym], options)
+
+      callbacks.before_send.call(request) if (callbacks && callbacks.before_send.respond_to?(:call))
       response = request.run
-      
-      Thread.current[:last_response] = response 
+      callbacks.on_complete.call(request.response) if (callbacks && callbacks.on_complete.respond_to?(:call))
+
+      Thread.current[:last_response] = response
 
       parse_response(response)
     end
-    
 
-    private 
+
+    private
 
     def self.set_options_for_environment(env, options)
       if env.kind_of?ApiBanking::Environment::YBL::PRD
@@ -58,7 +61,7 @@ module ApiBanking
     end
 
 
-        
+
     def self.parse_response(response)
       if response.success?
         if response.headers['Content-Type'] =~ /xml/ then
@@ -66,41 +69,41 @@ module ApiBanking
         end
       elsif response.timed_out?
         return Fault.new("502", "", "#{response.return_message}")
-      elsif response.code == 0 
+      elsif response.code == 0
         return Fault.new(response.code, "", response.return_message)
       else
         # http status indicating error, is either a datapower failure or a soap fault
         if response.headers['Content-Type'] =~ /xml/ then
            reply = Nokogiri::XML(response.response_body)
-           
+
            # datapower failures return an xml
            unless reply.at_xpath('//errorResponse').nil? then
              return parse_dp_reply(reply)
            end
-           
+
            # has to be a soapfault
            return parse_fault(reply)
-           
+
         end
         return Fault.new("#{response.code.to_s}", "", response.status_message)
       end
     end
-    
+
     def self.parse_dp_reply(reply)
       code = content_at(reply.at_xpath('/errorResponse/httpCode'))
       reasonText = content_at(reply.at_xpath('/errorResponse/moreInformation'))
       return Fault.new(code, "", reasonText)
     end
-    
+
     def self.content_at(node)
       node.content unless node.nil?
     end
-    
+
   end
-  
+
   class Soap12Client < SoapClient
-    private 
-    
+    private
+
     def self.add_soap_headers(options, soapAction)
       options[:headers] = {'Content-Type' => "application/xml; charset=utf-8"}
 
@@ -108,31 +111,31 @@ module ApiBanking
       # options[:headers][:SOAPAction] = data.doc.at_xpath('/soapenv12:Envelope/soapenv12:Body/*', 'soapenv12' => 'http://www.w3.org/2003/05/soap-envelope').name
 
     end
-    
+
     def self.construct_envelope(&block)
       Nokogiri::XML::Builder.new do |xml|
         xml.Envelope("xmlns:soap12" => "http://www.w3.org/2003/05/soap-envelope",
                      "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
                      "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema") do
           xml.parent.namespace = xml.parent.namespace_definitions.first
-          xml['soap12'].Header 
+          xml['soap12'].Header
           xml['soap12'].Body(&block)
         end
       end
-    end  
-    
+    end
+
     def self.parse_fault(reply)
       code   = content_at(reply.at_xpath('//soapenv12:Fault/soapenv12:Code/soapenv12:Subcode/soapenv12:Value', 'soapenv12' => 'http://www.w3.org/2003/05/soap-envelope'))
       subcode   = content_at(reply.at_xpath('//soapenv12:Fault/soapenv12:Code/soapenv12:Subcode/soapenv12:Subcode/soapenv12:Value', 'soapenv12' => 'http://www.w3.org/2003/05/soap-envelope'))
       reasonText   = content_at(reply.at_xpath('//soapenv12:Fault/soapenv12:Reason/soapenv12:Text', 'soapenv12' => 'http://www.w3.org/2003/05/soap-envelope'))
-      
+
       code ||= 'ns:E500'  # in certain cases, a fault code isn't set by the server
       return Fault.new(code, subcode, reasonText)
     end
 
-    
+
   end
-  
+
   class Soap11Client < SoapClient
     private
 
@@ -149,20 +152,20 @@ module ApiBanking
                      "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
                      "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema") do
           xml.parent.namespace = xml.parent.namespace_definitions.first
-          xml['soap11'].Header 
+          xml['soap11'].Header
           xml['soap11'].Body(&block)
         end
       end
-    end  
-    
+    end
+
     def self.parse_fault(reply)
       code   = nil # soap11 fault codes are meaningless
       reasonText   = content_at(reply.at_xpath('//soapenv11:Fault/faultstring', 'soapenv11' => 'http://schemas.xmlsoap.org/soap/envelope/'))
-      
+
       code ||= 'ns:E500'  # in certain cases, a fault code isn't set by the server
       return Fault.new(code, nil, reasonText)
     end
-    
-     
+
+
   end
 end
