070fc98cb18f2c5c499e98f95d393431e48fbf01
divyajayan <divya.jayan@quantiguous.com>
2017-03-24 12:12:27 +0530

    RBL api for account balance enquiry

--- lib/api_banking/environment/rbl/env.rb
+++ lib/api_banking/environment/rbl/env.rb
@@ -7,7 +7,8 @@ module ApiBanking
           self.ssl_ca_file ||= File.expand_path('./uat.pem', File.dirname(__FILE__))
           self.endpoints = {
             SinglePayment: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/payments/corp/payment',
-            AccountStatement: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/cas/statement'
+            AccountStatement: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/cas/statement',
+            GetAccountBalance: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/accounts/balance/query'
           }
         end
       end
