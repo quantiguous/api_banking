73147092114e1ea538508c8d8e4cc547622d3b63
divyajayan <divya.jayan@quantiguous.com>
2017-04-19 18:30:03 +0530

    added prod urls for rbl

--- lib/api_banking/environment/rbl/env.rb
+++ lib/api_banking/environment/rbl/env.rb
@@ -13,6 +13,19 @@ module ApiBanking
           }
         end
       end
+
+      PROD = Struct.new(:user, :password, :client_id, :client_secret, :ssl_client_cert, :ssl_client_key, :ssl_ca_file, :endpoints) do
+        def initialize(*)
+          super
+          self.ssl_ca_file ||= File.expand_path('./uat.pem', File.dirname(__FILE__))
+          self.endpoints = {
+            SinglePayment: 'https://gateway.rblbank.com/prod/sb/rbl/v1/payments/corp/payment',
+            AccountStatement: 'https://gateway.rblbank.com/prod/sb/rbl/v1/cas/statement',
+            GetAccountBalance: 'https://gateway.rblbank.com/prod/sb/rbl/v1/accounts/balance/query',
+            GetPaymentStatus: 'https://gateway.rblbank.com/prod/sb/rbl/v1/payments/corp/payment/query'
+          }
+        end
+      end
     end
   end  
 end
