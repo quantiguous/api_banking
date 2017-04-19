cb54df5078074e0b850207af677d24c0b9a6a49b
divyajayan <divya.jayan@quantiguous.com>
2017-04-19 18:48:20 +0530

    missed change

--- lib/api_banking/environment/rbl/env.rb
+++ lib/api_banking/environment/rbl/env.rb
@@ -4,7 +4,7 @@ module ApiBanking
       UAT = Struct.new(:user, :password, :client_id, :client_secret, :ssl_client_cert, :ssl_client_key, :ssl_ca_file, :endpoints) do
         def initialize(*)
           super
-          self.ssl_ca_file ||= File.expand_path('./uat.pem', File.dirname(__FILE__))
+          self.ssl_ca_file ||= File.expand_path('./prd.pem', File.dirname(__FILE__))
           self.endpoints = {
             SinglePayment: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/payments/corp/payment',
             AccountStatement: 'https://apideveloper.rblbank.com/test/sb/rbl/v1/cas/statement',
@@ -17,7 +17,7 @@ module ApiBanking
       PROD = Struct.new(:user, :password, :client_id, :client_secret, :ssl_client_cert, :ssl_client_key, :ssl_ca_file, :endpoints) do
         def initialize(*)
           super
-          self.ssl_ca_file ||= File.expand_path('./uat.pem', File.dirname(__FILE__))
+          self.ssl_ca_file ||= File.expand_path('./prd.pem', File.dirname(__FILE__))
           self.endpoints = {
             SinglePayment: 'https://gateway.rblbank.com/prod/sb/rbl/v1/payments/corp/payment',
             AccountStatement: 'https://gateway.rblbank.com/prod/sb/rbl/v1/cas/statement',
